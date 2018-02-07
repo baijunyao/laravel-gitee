5b404ebade8d5515e52e8b316b9b0c94d670a3e7
baijunyao <baijunyao@baijunyao.com>
2018-02-07 14:14:30 +0800

    :bug:修复php不在环境变量下时无法composer的情况

--- src/Controller/GiteeController.php
+++ src/Controller/GiteeController.php
@@ -4,6 +4,10 @@ namespace Baijunyao\LaravelGitee\Controller;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Artisan;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\PhpExecutableFinder;
 
 class GiteeController extends Controller
 {
@@ -17,26 +21,40 @@ class GiteeController extends Controller
         $data = $request->all();
         if (env('GITEE_HOOK_DEBUG') || ($data['password'] === env('GITEE_HOOK_PASSWORD') && 'merged' === $data['state'])) {
             $basePath = base_path();
+            chdir($basePath);
+            $phpBinPath = dirname((new PhpExecutableFinder)->find());
+
             // 回滚
             $reset = <<<EOF
-cd $basePath
 git add .
 git reset --hard HEAD^
 EOF;
             // 拉取命令
             $pull = <<<EOF
-cd $basePath
 git pull
+export PATH=\$PATH:$phpBinPath
 composer install --no-dev
-php artisan migrate --force
 EOF;
+            // 为了防止有新增的文件 先reset
+            $process = new Process($reset);
+            $process->run();
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+            dump($process->getOutput());
 
-            //为了防止有新增的文件 先reset
-            exec($reset, $result);
-            dump($result);
-            // 拉取代码
-            exec($pull, $result);
-            dump($result);
+            // 拉取代码 执行 composer install
+            $process = new Process($pull);
+            $process->run();
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+            dump($process->getOutput());
+
+            // 执行迁移
+            Artisan::call('migrate', [
+                '--force' => true,
+            ]);
         }
     }
 }
